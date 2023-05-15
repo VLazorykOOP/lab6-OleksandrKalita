89af0724fddf99d18630a6cd5e1a488800507635
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-05-15 10:49:18 +0200

    new commit
    adding function to class

--- OOPLab6T/OOPLab6T.cpp
+++ OOPLab6T/OOPLab6T.cpp
@@ -11,28 +11,38 @@ public:
 
 class Parallelepiped : public Shape {
 public:
-  double  width, height;
-  Parallelepiped( double w, double h) {
-    width = w;
-    height = h;
-  }
+    double  width, height;
+    Parallelepiped( double w, double h) {
+        width = w;
+        height = h;
+    }
+    double getSurfaceArea() {
+        return 6 * width * height;
+    }
 };
 
 class Tetrahedron : public Shape {
 public:
-  double side;
-  Tetrahedron(double s) {
-    side = s;
-  }
+    double side;
+    Tetrahedron(double s) {
+        side = s;
+    }
+    double getSurfaceArea() {
+        return pow(side, 2) * pow(3.14159, 3);
+    }
 };
 
 class Sphere : public Shape {
 public:
-  double radius;
-  Sphere(double r) {
-    radius = r;
-  }
+    double radius;
+    Sphere(double r) {
+        radius = r;
+    }
+    double getSurfaceArea() {
+        return 4 * 3.14159 * pow(radius, 2);
+    }
 };
+
 
 int main() {
 
