384a5e9b17169d384e73c06258a61c3b504b6ec0
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-05-15 10:43:49 +0200

    first commit
    virtual function

--- OOPLab6T/OOPLab6T.cpp
+++ OOPLab6T/OOPLab6T.cpp
@@ -1,20 +1,17 @@
-﻿// OOPLab3Tpl.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №6. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
 
-// Ваші файли загловки 
-//
-#include "Lab6Example.h"
-int main()
-{
-    std::cout << " Lab #6  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+class Shape {
+public:
+    virtual double getSurfaceArea() = 0; 
+};
 
-    Example();
 
+
+int main() {
+
+
+    return 0;
 }
