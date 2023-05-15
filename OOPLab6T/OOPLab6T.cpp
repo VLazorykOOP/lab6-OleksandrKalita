0e1dca509ec50928b91436dd8203a5a4bd58136b
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-05-15 10:46:25 +0200

    new one
    adding new classes

--- OOPLab6T/OOPLab6T.cpp
+++ OOPLab6T/OOPLab6T.cpp
@@ -9,6 +9,30 @@ public:
 };
 
 
+class Parallelepiped : public Shape {
+public:
+  double  width, height;
+  Parallelepiped( double w, double h) {
+    width = w;
+    height = h;
+  }
+};
+
+class Tetrahedron : public Shape {
+public:
+  double side;
+  Tetrahedron(double s) {
+    side = s;
+  }
+};
+
+class Sphere : public Shape {
+public:
+  double radius;
+  Sphere(double r) {
+    radius = r;
+  }
+};
 
 int main() {
 
